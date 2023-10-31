7d19335a9f296e3808c11bea8ea6a239e4be8a6d
Siphephelo Khanyile <Siphephelokhanyile@gmail.com>
2023-11-01 01:12:44 +0200

    adding signedness to last digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
+
 num = abs(number) % 10
+if number < 0:
+    num = -num
 if num > 5:
     print(f"Last digit of {number} is {num} and is greater than 5")
 elif num == 0:
