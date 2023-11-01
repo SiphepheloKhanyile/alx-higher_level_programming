26150cb982603986d029cdd9addf7f20f4346db8
Siphephelo Khanyile <Siphephelokhanyile@gmail.com>
2023-11-01 03:40:11 +0200

    changing formatting

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,7 +3,7 @@ def uppercase(str):
     for a in str:
         if ord(a) >= 97 and ord(a) <= 122:
             var = ord(a) - 32
-            print(chr(var), end="")
+            print("{}".format(chr(var)), end="")
         else:
-            print(a, end="")
+            print("{}".format(a), end="")
     print()
