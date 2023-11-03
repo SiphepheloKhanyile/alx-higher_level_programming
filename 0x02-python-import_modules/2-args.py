c18c8fddac700e1209cb79430571be73118e6f72
Siphephelo Khanyile <Siphephelokhanyile@gmail.com>
2023-11-03 02:16:53 +0200

    reformatting arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,6 +4,9 @@ if __name__ == "__main__":
     a = 1
     if len(sys.argv) < 2:
         print("0 arguments.")
+    elif len(sys.argv) == 2:
+        print("{} argument:".format(len(sys.argv) - 1))
+        print("{}: {}".format(a, sys.argv[1]))
     else:
         print("{} arguments:".format(len(sys.argv) - 1))
         for i in sys.argv[1:]:
