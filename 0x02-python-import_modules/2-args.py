ba03356dfa9e73ede680513527714c1a0689b8fe
Siphephelo Khanyile <Siphephelokhanyile@gmail.com>
2023-11-03 02:10:22 +0200

    adding number of arguments statement to output

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,6 +5,7 @@ if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("0 arguments.")
     else:
+        print("{} arguments:".format(len(sys.argv) - 1))
         for i in sys.argv[1:]:
             print("{}: {}".format(a, i))
             a += 1
