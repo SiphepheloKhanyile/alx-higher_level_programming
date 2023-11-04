e89b3837c2083f2a09389428c8577779be7c3904
Siphephelo Khanyile <Siphephelokhanyile@gmail.com>
2023-11-04 20:59:26 +0200

    changing logic

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,22 +1,20 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
     if len(tuple_a) < 2:
-        if len(tuple_a) < 1:
-            a = 0 + tuple_b[0]
+        if len(tuple_a) >= 1:
+            a = tuple_a[0] + tuple_b[0]
             b = 0 + tuple_b[1]
         else:
-            a = tuple_a[0] + tuple_b[0]
-            b = tuple_b[1] + 0
-        #a = 0 + tuple_b[0]
-        #b = 0 + tuple_b[1]
+            a = 0 + tuple_b[0]
+            b = 0 + tuple_b[1]
         return (a, b)
     elif len(tuple_b) < 2:
-        if len(tuple_b) < 1:
-            a = 0 + tuple_a[0]
+        if len(tuple_b) >= 1:
+            a = tuple_b[0] + tuple_a[0]
             b = 0 + tuple_a[1]
         else:
-            a = tuple_a[0] + tuple_b[0]
-            b = tuple_a[1] + 0
+            a = 0 + tuple_a[0]
+            b = 0 + tuple_a[1]
         return (a, b)
     else:
         a = tuple_a[0] + tuple_b[0]
