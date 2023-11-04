87127e072d71dee66eb3a8003133056e7c1c2811
Siphephelo Khanyile <Siphephelokhanyile@gmail.com>
2023-11-04 22:22:39 +0200

    removing None from tuple

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if len(sentence) == 0:
-        return (None, )
+        return None
     else:
         return_t = (len(sentence), sentence[0])
         return return_t
