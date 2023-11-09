f8c2058feb41cded4fb4844cd7d4e193acdfb7dc
Siphephelo Khanyile <Siphephelokhanyile@gmail.com>
2023-11-09 06:26:09 +0200

    reducing number of lines in code

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-    ls = my_list.copy()
-    return_lis = list((map(lambda x: x * number, ls)))
-    return (return_lis)
+    return (list((map(lambda x: x * number, my_list))))
