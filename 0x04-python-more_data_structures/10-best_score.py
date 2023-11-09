656beafc25d91877987058dea0870e9240f8d1a6
Siphephelo Khanyile <Siphephelokhanyile@gmail.com>
2023-11-09 06:36:44 +0200

    confusing checker

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None:
+    if a_dictionary is None or len(a_dictionary) < 1:
         return None
     biggest = 0
     for x, y in a_dictionary.items():
