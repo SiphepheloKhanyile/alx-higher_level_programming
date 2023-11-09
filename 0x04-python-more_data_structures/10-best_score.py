0f1c24cab3b2c6ce660fbeb2393472502d2487a6
Siphephelo Khanyile <Siphephelokhanyile@gmail.com>
2023-11-09 06:04:29 +0200

    correcting logic

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,10 +2,11 @@
 def best_score(a_dictionary):
     if a_dictionary is None:
         return None
-    for x, y in sorted(a_dictionary.items()):
-        biggest = y
-        key = x
-        if y > biggest:
-            biggest = y
-            key = x
+    else:
+        biggest = 0
+        key = ""
+        for x, y in a_dictionary.items():
+            if y > biggest:
+                biggest = y
+                key = x
     return (key)
