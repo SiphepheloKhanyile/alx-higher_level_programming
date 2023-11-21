3126ec11bee8d705291f502edaca717ba0263761
Siphephelo Khanyile <Siphephelokhanyile@gmail.com>
2023-11-21 04:30:13 +0200

    assigning error to variable

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -2,7 +2,8 @@
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
-    except Exception:
+    except Exception as err:
+        print("Exception: ", err)
         return (False)
     else:
         return (True)
