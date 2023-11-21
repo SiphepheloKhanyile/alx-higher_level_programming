6419b7564426d8dfe34c7556c01193ab4fc72813
Siphephelo Khanyile <Siphephelokhanyile@gmail.com>
2023-11-21 02:35:23 +0200

    catching errors with Exception

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,8 +5,7 @@ def safe_print_list(my_list=[], x=0):
         while (count < x):
             print("{}".format(my_list[count]), end="")
             count += 1
-    except IndexError:
+    except Exception:
         pass
-    finally:
-        print('\n')
+    print('\n')
     return (count)
