172a3364ab7e1f72d65adc5e71538cea43bf0a41
Siphephelo Khanyile <Siphephelokhanyile@gmail.com>
2023-11-21 04:09:14 +0200

    changing exception type

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -14,7 +14,7 @@ def list_division(my_list_1, my_list_2, list_length):
             result = 0
             new_list.append(result)
             print("division by 0")
-        except TypeError:
+        except Exception:
             result = 0
             new_list.append(result)
             print("wrong type")
