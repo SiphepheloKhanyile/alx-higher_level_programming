e08cd739a676a1c9f27839de619be371d0286224
Siphephelo Khanyile <Siphephelokhanyile@gmail.com>
2023-11-24 04:26:06 +0200

    a class Square that defines a square by: (based on 5-square.py)

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -10,8 +10,8 @@ class Square():
             size(int): size of square, deafualt = 0
             position(tuple): optional positional arguement
         """
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
@@ -49,8 +49,12 @@ class Square():
         if (self.__size == 0):
             print("")
             return
-        [print("") for i in range(0, self.__position[1])]
-        for i in range(0, self.__size):
-            [print(" ", end="") for j in range(0, self.__position[0])]
-            [print("#", end="") for k in range(0, self.__size)]
-            print("")
+        else:
+            for a in range(self.__position[1]):
+                print("")
+            for b in range(self.__size):
+                for c in range(self.__position[0]):
+                    print(" ", end="")
+                for d in range(self.__size):
+                    print("#", end="")
+                print("")
