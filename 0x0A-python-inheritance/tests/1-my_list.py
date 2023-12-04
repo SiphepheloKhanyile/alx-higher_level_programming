b2e63b418b7c489e1c32ceba83c8324ebd112480
Siphephelo Khanyile <Siphephelokhanyile@gmail.com>
2023-12-04 23:42:48 +0200

    test documnentation fix

--- 0x0A-python-inheritance/tests/1-my_list.py
+++ 0x0A-python-inheritance/tests/1-my_list.py
@@ -4,20 +4,6 @@
 
 class MyList(list):
     """class that inherits from 'list'"""
-    def __init__(self, lst=[]):
-        """Initialising object"""
-        super().__init__(lst)
-        self.lst = lst
-
-    @property
-    def lst(self):
-        """getter and setter for lst"""
-        return lst
-
-    @lst.setter
-    def lst(self, value):
-        self.__lst = value
-
     def print_sorted(self):
         """function that prints the list, sorted (ascending sort)"""
         print(sorted(self))
