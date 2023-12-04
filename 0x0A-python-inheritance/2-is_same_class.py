19898596eef5b330b15dc00b18130851d26994b4
Siphephelo Khanyile <Siphephelokhanyile@gmail.com>
2023-12-04 23:59:09 +0200

    a function that returns True if the object is exactly an instance of the specified class ;

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""a function that returns True if the object is exactly
+an instance of the specified class
+"""
+
+
+def is_same_class(obj, a_class):
+    """ returns True if the object is exactly
+        an instance of the specified class
+    Args:
+        obj (object): object to be checked
+        a_class (class): class to be compared to
+    Return:
+        True if obj is instance of a_class
+        else Retrun False
+    """
+    if (isinstance(type(obj).__name__, a_class) is True):
+        return True
+    if (isinstance(type(obj).__name__, a_class) is False):
+        return False
