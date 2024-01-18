305c8d5fcd44e3395bfb98463c3861483ebe00f7
Siphephelo Khanyile <siphephelokhanyile@gmail.com>
2024-01-18 22:37:16 +0200

    removing 'number' from quotes

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -3,7 +3,7 @@
 --		the score
 --		number of records for this score with the label number
 -- list should be sorted by the number of records (descending)
-SELECT score, COUNT(*) AS 'number'
+SELECT score, COUNT(*) AS number
 FROM second_table
 GROUP BY score
-ORDER BY 'number' DESC;
+ORDER BY number DESC;
