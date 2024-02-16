35ae69e25647dd686143dbaa69b988c4be1fbbcf
Siphephelo Khanyile <siphephelokhanyile@gmail.com>
2024-02-16 23:50:09 +0200

    fix : rewriting logic

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,10 +1,4 @@
 #!/usr/bin/node
 exports.converter = function (base) {
-  return function convertToBaseN (number) {
-    if (number === 0) {
-      return '';
-    } else {
-      return convertToBaseN(Math.floor(number / base)) + '0123456789abcdef'[number % base];
-    }
-  };
+  return num => num.toString(base);
 };
