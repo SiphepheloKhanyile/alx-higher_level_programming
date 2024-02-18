193b4798952797eb0e342b0a5db706bd860aacd6
Siphephelo Khanyile <siphephelokhanyile@gmail.com>
2024-02-18 05:20:43 +0200

    add : a script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument.

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+a script that takes in an argument
+displays all values in the 'states' table of hbtn_0e_0_usa
+where 'name' matches the argument.
+"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    name_searched = sys.argv[4]
+
+    conn = MySQLdb.connect(
+        host='localhost',
+        user=username,
+        passwd=password,
+        db=database,
+        port=3306,
+        charset="utf8"
+    )
+
+    curs = conn.cursor()
+    curs.execute("SELECT * FROM states WHERE name = %s;", (name_searched,))
+    rows = curs.fetchall()
+    for row in rows:
+        print(row)
