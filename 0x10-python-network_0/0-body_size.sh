74da75922c92a818cfb1713532ec8244613186c1
Siphephelo Khanyile <siphephelokhanyile@gmail.com>
2024-03-01 03:05:07 +0200

    fix : checker fix, refactor code to meet checker requirements

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # takes in a URL, sends a request to that URL
 # and displays the size of the body of the response
-curl -s "$1" | wc -c
+SIZE=$(curl -sI "$1" | grep -i "content-length" | awk '{print $2}')
+echo "$SIZE"
