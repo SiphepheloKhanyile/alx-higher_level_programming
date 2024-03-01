0414704d426c12223350e57f61e51385af29c666
Siphephelo Khanyile <siphephelokhanyile@gmail.com>
2024-03-01 03:06:44 +0200

    fix : checker fix, refactor code to meet checker requirements, shortened number of lines in script

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# takes in a URL, sends a request to that URL
-# and displays the size of the body of the response
+# takes in a URL, sends a request to that URL and displays the size of the body of the response
 SIZE=$(curl -sI "$1" | grep -i "content-length" | awk '{print $2}')
 echo "$SIZE"
