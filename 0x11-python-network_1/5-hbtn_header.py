433399fe03386f9ee9d182ef29f47ded7742af09
Siphephelo Khanyile <siphephelokhanyile@gmail.com>
2024-03-02 04:52:02 +0200

    fix : removing timeout parameter

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -10,5 +10,5 @@ import requests
 if __name__ == "__main__":
     url = sys.argv[1]
 
-    response = requests.get(url, timeout=None)
+    response = requests.get(url)
     print(response.headers['X-Request-Id'])
