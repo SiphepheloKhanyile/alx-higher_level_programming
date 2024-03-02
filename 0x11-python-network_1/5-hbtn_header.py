e69d6ef0562dad87c157b7045540c02933cc02ee
Siphephelo Khanyile <siphephelokhanyile@gmail.com>
2024-03-02 05:02:12 +0200

    fix : adding 'get' method

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -10,5 +10,5 @@ import requests
 if __name__ == "__main__":
     url = sys.argv[1]
 
-    response = requests.get(url)
-    print(response.headers['X-Request-Id'])
+    response = requests.get(url, timeout=None)
+    print(response.headers.get('X-Request-Id'))
