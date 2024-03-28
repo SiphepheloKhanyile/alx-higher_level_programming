7a798aa280807d97488705a82055ae43683bca81
Siphephelo Khanyile <siphephelokhanyile@gmail.com>
2024-03-28 23:13:06 +0200

    add: js script that display the status code of a GET request.

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const request = require('request');
+
+// Get the URL from command line arguments
+const url = process.argv[2];
+
+// Make a GET request to the provided URL
+request.get(url, (error, response) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+  console.log(`code: ${response.statusCode}`);
+});
